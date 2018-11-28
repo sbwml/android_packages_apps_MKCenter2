6a26a9999c0499a5dc2a80104d4bdbc588f44f7e
Martincz Gao <martincz@mokeedev.com>
2018-11-28 13:16:30 +0800

    MKCenter2: cleanup
    Change-Id: I6314af643f326d1f24e255a61612e69bac123775

--- app/src/main/java/com/mokee/center/activity/MainActivity.java
+++ app/src/main/java/com/mokee/center/activity/MainActivity.java
@@ -164,7 +164,7 @@ public class MainActivity extends AppCompatActivity
         }
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
+        return false;
     }
 
     public Snackbar makeSnackbar(CharSequence text) {
